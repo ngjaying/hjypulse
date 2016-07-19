fcdc311a68a9d672a40fc4a72eb67308cb839881
olln3 <johnnyyellow@gmail.com>
2016-07-19 18:26:40 +0800

    update main and login page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,21 +1,5 @@
 'use strict';
-
-(function() {
-
-  class MainController {
-
-    constructor($http) {
-      this.$http = $http;
-    }
-
-    $onInit() {
-      
-    }
-  }
-
-  angular.module('pulseApp')
-    .component('main', {
-      templateUrl: 'app/main/main.html',
-      controller: MainController
-    });
-})();
+angular.module('pulseApp')
+  .component('main', {
+    templateUrl: 'app/main/main.html'
+  });
